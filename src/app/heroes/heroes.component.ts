9496f7b15166de6cb071bae008381fe218da24c2
Ahmed Nasr <ahmednasr.fci97@gmail.com>
2023-05-30 23:52:09 +0300

    push the project

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,28 @@
+import { MessageService } from './../message.service';
+import { Component } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css'],
+})
+export class HeroesComponent {
+  selectedHero?: Hero;
+  heroes: Hero[] = [];
+
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) {}
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+  }
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+  ngOnInit() {
+    this.getHeroes();
+  }
+}
